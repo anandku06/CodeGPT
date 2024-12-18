a0932d9fded346da764a2dff18e131198ae720ad
Commiter <161126335+anandku06@users.noreply.github.com>
2024-12-18 18:58:24 +0530

    add API key and URL for OpenAI chat completions

---
+++ index.js
@@ -0,0 +1,3 @@
+const apiKey = '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'
+
+const apiURL = 'https://api.openai.com/v1/chat/completions'
